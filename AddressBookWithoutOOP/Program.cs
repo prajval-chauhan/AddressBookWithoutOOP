6d690e8e38bf2fbf7ef2dff95010a3e90bfb7d06
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-06 04:31:44 +0530

    moved the contents of the class Contact Details into main class but now the list isnt storing any values

--- AddressBookWithoutOOP/Program.cs
+++ AddressBookWithoutOOP/Program.cs
@@ -16,11 +16,11 @@ namespace AddressBookWithoutOOP
                 switch(input)
                 {
                     case 1:
-                        ContactDetails Call = new ContactDetails();
+                        Program Call = new Program();
                         Call.AddContact();
                         break;
                     case 2:
-                        ContactDetails Calll = new ContactDetails();
+                        Program Calll = new Program();
                         Calll.DisplayRecords();
                         break;
                 }
@@ -28,72 +28,78 @@ namespace AddressBookWithoutOOP
                     break;
             }
         }
+        public List<ContactDetails> Records = new List<ContactDetails>();
 
-        public class ContactDetails
+        public void AddContact()
         {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string PhoneNumber { get; set; }
-            public string Address { get; set; }
-            public string ZipCode { get; set; }
-            public string State { get; set; }
-            public string Email { get; set; }
+            Console.Clear();
+            ContactDetails contact = new ContactDetails();
 
-            public List<ContactDetails> Records = new List<ContactDetails>();
+            Console.WriteLine("Enter the first name: ");
+            contact.FirstName = Console.ReadLine();
 
-            public void AddContact()
+            Console.WriteLine("Enter the last name: ");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter the phone number: ");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address: ");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the ZIP code: ");
+            contact.ZipCode = Console.ReadLine();
+
+            Console.WriteLine("Enter the State: ");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the eMail address: ");
+            contact.Email = Console.ReadLine();
+
+            Records.Add(contact);
+        }
+
+        public void DisplayContact(ContactDetails contact)
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("First Name : " + contact.FirstName);
+            Console.WriteLine("Last Name : " + contact.LastName);
+            Console.WriteLine("Phone Number : " + contact.PhoneNumber);
+            Console.WriteLine("Address : " + contact.Address);
+            Console.WriteLine("ZIP Code : " + contact.ZipCode);
+            Console.WriteLine("State : " + contact.State);
+            Console.WriteLine("Email ID : " + contact.Email);
+            Console.WriteLine("-----------------------------------");
+
+        }
+        public void DisplayRecords()
+        {
+            if (Records.Count == 0)
             {
-                ContactDetails contact = new ContactDetails();
-
-                Console.WriteLine("Enter the first name: ");
-                contact.FirstName = Console.ReadLine();
-
-                Console.WriteLine("Enter the last name: ");
-                contact.LastName = Console.ReadLine();
-
-                Console.WriteLine("Enter the phone number: ");
-                contact.PhoneNumber = Console.ReadLine();
-
-                Console.WriteLine("Enter the Address: ");
-                contact.Address = Console.ReadLine();
-
-                Console.WriteLine("Enter the ZIP code: ");
-                contact.ZipCode = Console.ReadLine();
-
-                Console.WriteLine("Enter the State: ");
-                contact.State = Console.ReadLine();
-
-                Console.WriteLine("Enter the eMail address: ");
-                contact.Email = Console.ReadLine();
-
-                Records.Add(contact);
+                Console.WriteLine("The records are empty");
+                return;
             }
-
-            public void DisplayContact(ContactDetails contact)
+            Console.WriteLine("RECORDS: " + "\n----------");
+            foreach (var Contact in Records)
             {
-                Console.WriteLine("First Name : " + contact.FirstName);
-                Console.WriteLine("Last Name : " + contact.LastName);
-                Console.WriteLine("Phone Number : " + contact.PhoneNumber);
-                Console.WriteLine("Address : " + contact.Address);
-                Console.WriteLine("ZIP Code : " + contact.ZipCode);
-                Console.WriteLine("State : " + contact.State);
-                Console.WriteLine("Email ID : " + contact.Email);
-
+                DisplayContact(Contact);
             }
-            public void DisplayRecords()
-            {
-                if (Records.Count == 0)
-                {
-                    Console.WriteLine("The records are empty");
-                    return;
-                }
-                    Console.WriteLine("RECORDS: " + "\n----------");
-                    foreach (var Contact in Records)
-                    {
-                        DisplayContact(Contact);
-                    }
-                }
-            }
+            Console.ReadKey();
         }
     }
+    public class ContactDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+
+
+    }
+
+
+}
 
