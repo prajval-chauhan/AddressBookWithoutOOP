05b511e2d31f4a3592811275a223f69d8f2c46e5
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-06 04:11:04 +0530

    Added methods to display and add contacts. The program is running but isn't displaying the list of contacts

--- AddressBookWithoutOOP/Program.cs
+++ AddressBookWithoutOOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBookWithoutOOP
 {
@@ -7,8 +8,92 @@ namespace AddressBookWithoutOOP
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program\n------------------------------------ ");
+            for (; ; )
+            {
+                Console.WriteLine("1. Add a new contact \n2. Display all the contacts\n3. Exit \nenter your choice...");
+                int input = Convert.ToInt16(Console.ReadLine());
+
+                switch(input)
+                {
+                    case 1:
+                        ContactDetails Call = new ContactDetails();
+                        Call.AddContact();
+                        break;
+                    case 2:
+                        ContactDetails Calll = new ContactDetails();
+                        Calll.DisplayRecords();
+                        break;
+                }
+                if (input == 3)
+                    break;
+            }
+        }
+
+        public class ContactDetails
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Address { get; set; }
+            public string ZipCode { get; set; }
+            public string State { get; set; }
+            public string Email { get; set; }
+
+            public List<ContactDetails> Records = new List<ContactDetails>();
+
+            public void AddContact()
+            {
+                ContactDetails contact = new ContactDetails();
+
+                Console.WriteLine("Enter the first name: ");
+                contact.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Enter the last name: ");
+                contact.LastName = Console.ReadLine();
+
+                Console.WriteLine("Enter the phone number: ");
+                contact.PhoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter the Address: ");
+                contact.Address = Console.ReadLine();
+
+                Console.WriteLine("Enter the ZIP code: ");
+                contact.ZipCode = Console.ReadLine();
+
+                Console.WriteLine("Enter the State: ");
+                contact.State = Console.ReadLine();
+
+                Console.WriteLine("Enter the eMail address: ");
+                contact.Email = Console.ReadLine();
+
+                Records.Add(contact);
+            }
+
+            public void DisplayContact(ContactDetails contact)
+            {
+                Console.WriteLine("First Name : " + contact.FirstName);
+                Console.WriteLine("Last Name : " + contact.LastName);
+                Console.WriteLine("Phone Number : " + contact.PhoneNumber);
+                Console.WriteLine("Address : " + contact.Address);
+                Console.WriteLine("ZIP Code : " + contact.ZipCode);
+                Console.WriteLine("State : " + contact.State);
+                Console.WriteLine("Email ID : " + contact.Email);
+
+            }
+            public void DisplayRecords()
+            {
+                if (Records.Count == 0)
+                {
+                    Console.WriteLine("The records are empty");
+                    return;
+                }
+                    Console.WriteLine("RECORDS: " + "\n----------");
+                    foreach (var Contact in Records)
+                    {
+                        DisplayContact(Contact);
+                    }
+                }
+            }
         }
     }
 
-
-}
